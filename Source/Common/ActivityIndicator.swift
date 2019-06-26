9f106c79b4f253615c80dc32aeb4eda431ca89eb
Eldis <duanlangc@gmail.com>
2019-06-26 14:53:05 +0800

    Swift5 Support

--- Source/Common/ActivityIndicator.swift
+++ Source/Common/ActivityIndicator.swift
@@ -16,16 +16,16 @@ import RxCocoa
 private struct ActivityToken<E> : ObservableConvertibleType, Disposable {
     private let _source: Observable<E>
     private let _dispose: Cancelable
-
+    
     init(source: Observable<E>, disposeAction: @escaping () -> ()) {
         _source = source
         _dispose = Disposables.create(with: disposeAction)
     }
-
+    
     func dispose() {
         _dispose.dispose()
     }
-
+    
     func asObservable() -> Observable<E> {
         return _source
     }
@@ -33,52 +33,55 @@ private struct ActivityToken<E> : ObservableConvertibleType, Disposable {
 
 /**
  Enables monitoring of sequence computation.
-
+ 
  If there is at least one sequence computation in progress, `true` will be sent.
  When all activities complete `false` will be sent.
  */
 public class ActivityIndicator : SharedSequenceConvertibleType {
-    public typealias E = Bool
+    
+    
+    public typealias Element = Bool
+    
     public typealias SharingStrategy = DriverSharingStrategy
-
+    
     private let _lock = NSRecursiveLock()
     private let _relay = BehaviorRelay(value: 0)
     private let _loading: SharedSequence<SharingStrategy, Bool>
-
+    
     public init() {
         _loading = _relay.asDriver()
             .map { $0 > 0 }
             .distinctUntilChanged()
     }
-
-    fileprivate func trackActivityOfObservable<O: ObservableConvertibleType>(_ source: O) -> Observable<O.E> {
-        return Observable.using({ () -> ActivityToken<O.E> in
+    
+    fileprivate func trackActivityOfObservable<O: ObservableConvertibleType>(_ source: O) -> Observable<O.Element> {
+        return Observable.using({ () -> ActivityToken<O.Element> in
             self.increment()
             return ActivityToken(source: source.asObservable(), disposeAction: self.decrement)
         }) { t in
             return t.asObservable()
         }
     }
-
+    
     private func increment() {
         _lock.lock()
         _relay.accept(_relay.value + 1)
         _lock.unlock()
     }
-
+    
     private func decrement() {
         _lock.lock()
         _relay.accept(_relay.value - 1)
         _lock.unlock()
     }
-
-    public func asSharedSequence() -> SharedSequence<SharingStrategy, E> {
+    
+    public func asSharedSequence() -> SharedSequence<DriverSharingStrategy, ActivityIndicator.Element> {
         return _loading
     }
 }
 
 extension ObservableConvertibleType {
-    public func trackActivity(_ activityIndicator: ActivityIndicator) -> Observable<E> {
+    public func trackActivity(_ activityIndicator: ActivityIndicator) -> Observable<Element> {
         return activityIndicator.trackActivityOfObservable(self)
     }
 }
